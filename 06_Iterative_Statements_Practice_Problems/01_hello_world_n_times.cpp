8acfed7034bcc72964c066dc9324ed3b099cd787
rohit528590 <rohit528590@icloud.com>
2025-10-24 06:53:46 +0530

    Solved practice question on Loops

--- 06_Iterative_Statements_Practice_Problems/01_hello_world_n_times.cpp
+++ 06_Iterative_Statements_Practice_Problems/01_hello_world_n_times.cpp
@@ -1,16 +1,17 @@
 // Program to print "Hello World" 'n' times, where 'n' is input from the user.
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
     int n;
 
-    printf("Enter how many times you want to print 'Hellow World' : ");
-    scanf("%d", &n);
+    cout << "Enter how many times you want to print 'Hellow World' : ";
+    cin >> n;
 
     for (int i = 1; i <= n; i++)
     {
-        printf("Hellow World\n");
+        cout << "Hellow World\n";
     }
 
     return 0;

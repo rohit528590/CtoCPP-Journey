14486063166823b1589344a0d942ac6ad402a122
rohit528590 <rohit528590@icloud.com>
2025-10-28 15:10:57 +0530

    Solved challenging Pattern Printing question from CtoCPP-Journey

---
+++ Superb_Questions/04_Pattern_Printing_Superb_Questions/01_rectangle_asterisk_pattern.cpp
@@ -0,0 +1,35 @@
+/*
+1. Rectangle Pattern with Asterisks
+Program to print a rectangle pattern using asterisks.
+Example : rows = 3, columns = 4
+Expected Output:
+* * * * *
+* * * * *
+* * * * * 
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, columns;
+
+    cout << "Enter the number of rows : ";
+    cin >> rows;
+
+    cout << "Enter the number of columns : ";
+    cin >> columns;
+
+    cout << "Rectangle(" << rows << " x " << columns << ") Pattern with Asterisks -:\n";
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= columns; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << "\n";
+    }
+
+    return 0; 
+}
